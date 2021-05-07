0fbe00479cdc55202116706683b6d1d5923760f2
Balint Laszlo <balint.laszlo@t-systems.com>
2021-05-07 09:16:16 +0200

    Add InitBinder

--- pet-clinic-web/src/main/java/hu/laszlobalint/spring/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/hu/laszlobalint/spring/petclinic/controller/OwnerController.java
@@ -3,7 +3,9 @@ package hu.laszlobalint.spring.petclinic.controller;
 import hu.laszlobalint.spring.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,6 +18,11 @@ public class OwnerController {
 
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder webDataBinder) {
+        webDataBinder.setDisallowedFields("id");
     }
 
     @GetMapping({"", "/", "/index", "/index.html"})
